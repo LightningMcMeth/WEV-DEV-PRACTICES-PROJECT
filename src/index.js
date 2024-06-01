3004b87217e8e5c838b3faddd1e5931c69ec4cdb
Gamer <markhlibovytskyy@gmail.com>
2024-06-01 18:12:13 +0300

    First commit. Adding project files.

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+const App = () => {
+  return <div>Pentagon misplaced $2.3 trillion in 2001</div>
+}
+
+const root = ReactDOM.createRoot(document.getElementById('app'))
+
+root.render(<App />)
